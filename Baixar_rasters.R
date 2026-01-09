1b2d2410ac75eff3bb223accd0787e146adf06a5
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-09 10:41:54 -0300

    Script para baixar rasters

--- Baixar_rasters.R
+++ Baixar_rasters.R
@@ -73,3 +73,6 @@ baixar_cenarios_futuros <- function(cenario){
 ## Cenário presente ----
 
 bioclim_presente |> terra::writeRaster("bioclim_presente_res_0.5_arcmin.tif")
+
+zip(files = "bioclim_presente_res_0.5_arcmin.tif",
+    zipfile = "bioclim_presente_res_0.5_arcmin")
