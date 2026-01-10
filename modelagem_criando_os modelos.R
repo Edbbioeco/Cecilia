8acc524b796e9af69ba0546a08c58d057d4af2eb
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-10 07:47:41 -0300

    Script para criar os modelos

---
+++ modelagem_criando_os modelos.R
@@ -0,0 +1,53 @@
+# Pacotes ----
+
+library(sf)
+
+library(tidyverse)
+
+library(terra)
+
+library(tidyterra)
+
+library(usdm)
+
+library(sdm)
+
+# Dados ----
+
+## Shapefile da Caatinga ----
+
+### Importando ----
+
+### Visualizando ----
+
+## Registros d ocorrência filtrados ----
+
+### Importando ----
+
+### Visualizando ----
+
+### Transformando em vetor espacial ----
+
+## Variáveis bioclimáticas para o presente ----
+
+### Importando ----
+
+### Visualizando ----
+
+## Variáveis bioclimáticas para o futuro ----
+
+### Importando ----
+
+### Visualizando ----
+
+# Recortando e excluindo as variáveis dos rasters ----
+
+## Recortando ----
+
+## Excluindo ----
+
+# Criando os modelos ----
+
+## Objeto sdmdata ----
+
+## Modelos sdm ----
