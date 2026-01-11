66d4b18120175bc981dfac649148eaabcfd467d5
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-11 09:59:18 -0300

    Script para a confecção dos mapas

---
+++ pos_modelagem_confeccao_mapas.R
@@ -0,0 +1,25 @@
+# Pacotes ----
+
+library(sf)
+
+library(tidyverse)
+
+library(terra)
+
+library(tidyterra)
+
+library(ggspatial)
+
+library(ggview)
+
+library(patchwork)
+
+# Dados ----
+
+# Mapa de área de probabilidade de ocorrência para o presente ----
+
+# Mapa de área de probabilidade de ocorrência para o futuro -----
+
+# Mapa de área de nicho para o presente ----
+
+# Mapa de área de nicho para o futuro -----
