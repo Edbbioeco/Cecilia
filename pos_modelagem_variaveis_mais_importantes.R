27467c0119e27715c1782b0e0d72ae32aed9030c
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-12 17:12:41 -0300

    Script para analisar as variáveis mais importantes para o modelo

--- pos_modelagem_variaveis_mais_importantes.R
+++ pos_modelagem_variaveis_mais_importantes.R
@@ -14,4 +14,18 @@ modelo_sdm <- sdm::read.sdm("modelo_sdm.sdm")
 
 modelo_sdm
 
+# Métricas do modelo ----
+
+modelo_sdm |> getEvaluation(stat = c("AUC", "TSS"))
+
+# Curva ROC ----
+
+modelo_sdm |> sdm::roc()
+
 # Importância das variáveis ----
+
+modelo_sdm |> sdm::getVarImp() |> plot() + theme_bw()
+
+# Curva de resposta ----
+
+modelo_sdm |> sdm::getResponseCurve() |> plot() + theme_bw()
