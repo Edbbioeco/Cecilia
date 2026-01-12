d66b41792cafa6b77667174d92bbdfa4e9e8a2b5
Edbbioeco <edsonbbiologia@gmail.com>
2026-01-12 17:00:57 -0300

    Script para analisar as variáveis mais importantes para o modelo

---
+++ pos_modelagem_variaveis_mais_importantes.R
@@ -0,0 +1,17 @@
+# Pacotes ----
+
+library(sdm)
+
+library(tidyverse)
+
+# Dados ----
+
+## Importando ----
+
+modelo_sdm <- sdm::read.sdm("modelo_sdm.sdm")
+
+## Visualizando ----
+
+modelo_sdm
+
+# Importância das variáveis ----
