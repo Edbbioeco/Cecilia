df54fdaf188cdea8fa41d0eab46970436d86b920
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-04 17:52:14 -0300

    Mapa de predição de ensemble para o presente

--- pos_modelagem_confeccao_mapas.R
+++ pos_modelagem_confeccao_mapas.R
@@ -319,3 +319,36 @@ ggplot() +
 
 ggsave(filename = "mapa_probabilidade_ocorrencia_passado.png",
        height = 10, width = 12)
+
+## Presente ----
+
+ggplot() +
+  geom_sf(data = br, aes(color = "Brasil"),
+          linewidth = 1) +
+  tidyterra::geom_spatraster(data = ensemble_presente) +
+  geom_sf(data = caatinga, aes(color = "Caatinga"),
+          fill = NA, linewidth = 0.75) +
+  scale_fill_viridis_c(na.value = NA,
+                       limits = c(0, 1),
+                       name = "Probabilidade ocorrência",
+                       guide = guide_colourbar(title.position = "top",
+                                               title.hjust = 0.5,
+                                               barwidth = 20,
+                                               frame.colour = "black",
+                                               ticks.colour = "black")) +
+  scale_color_manual(values = c("Brasil" = "black",
+                                "Caatinga" = "orangered"),
+                     name = NULL) +
+  geom_sf(data = br, linewidth = 1, fill = NA, color = "black") +
+  coord_sf(xlim = c(-45.07807, -35.06698),
+           ylim = c(-16.71256, -2.748381)) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 20, color = "black"),
+        legend.text = element_text(size = 20, color = "black"),
+        legend.title = element_text(size = 20, color = "black"),
+        legend.position = "bottom",
+        panel.border = element_rect(color = "black", linewidth = 1)) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "mapa_probabilidade_ocorrencia_presente.png",
+       height = 10, width = 12)
