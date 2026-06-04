24669f6518cb480365034554833139047f585a75
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-04 18:27:51 -0300

    Mapa de área de nicho para o futuro no cenário 585

--- pos_modelagem_confeccao_mapas.R
+++ pos_modelagem_confeccao_mapas.R
@@ -723,3 +723,46 @@ ggplot() +
 
 ggsave(filename = "mapa_area_nicho_futuro_cenario370.png",
        height = 10, width = 12)
+
+### Cenário 585 ----
+
+ggplot() +
+  geom_sf(data = br, aes(color = "Brasil")) +
+  tidyterra::geom_spatraster(data = area_nicho_futuro[
+    ensemble_futuro |>
+      names() |>
+      stringr::str_detect("585")] |>
+      terra::rast() |>
+      setNames(ensemble_futuro |>
+                 names() %>%
+                 .[ensemble_futuro |>
+                     names() |>
+                     stringr::str_detect("585")] |>
+                 stringr::str_remove("ensemble_futuro_585-"))) +
+  geom_sf(data = caatinga, aes(color = "Caatinga"),
+          fill = NA, linewidth = 0.75) +
+  scale_fill_viridis_d(na.translate = FALSE,
+                       direction = -1,
+                       name = "Nicho",
+                       guide = guide_legend(title.position = "top",
+                                            title.hjust = 0.5),
+                       breaks = c("Não área de nicho", "Área de nicho")) +
+  scale_color_manual(values = c("Brasil" = "black",
+                                "Caatinga" = "orangered"),
+                     name = NULL) +
+  geom_sf(data = br, linewidth = 0.75, fill = NA, color = "black") +
+  coord_sf(xlim = c(-45.07807, -35.06698),
+           ylim = c(-16.71256, -2.748381)) +
+  facet_wrap(~lyr) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 20, color = "black"),
+        legend.text = element_text(size = 20, color = "black"),
+        legend.title = element_text(size = 20, color = "black"),
+        legend.position = "bottom",
+        strip.text = element_text(size = 20, color = "black"),
+        strip.background = element_rect(color = "black", linewidth = 1),
+        panel.border = element_rect(color = "black", linewidth = 1)) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "mapa_area_nicho_futuro_cenario585.png",
+       height = 10, width = 12)
