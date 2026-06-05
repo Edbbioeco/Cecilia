25be4980ee16ebcd83f37c1059100af61aa2d6b6
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-05 11:00:18 -0300

    Criar a table flextable

--- pos_modelagem_variaveis_mais_importantes.R
+++ pos_modelagem_variaveis_mais_importantes.R
@@ -18,7 +18,30 @@ modelo_sdm
 
 # Métricas do modelo ----
 
-modelo_sdm |> getEvaluation(stat = c("AUC", "TSS"))
+## Tabela da esttísticas ----
+
+### Criar a tabela ----
+
+tabela_auc_tss_flex <- modelo_sdm |>
+  getEvaluation(stat = c("AUC", "TSS")) |>
+  dplyr::mutate(Modelo = rep(c("gam",
+                               "glm",
+                               "maxent",
+                               "maxlike"),
+                             each = 5),
+                .before = modelID,
+                AUC = AUC |> round(2),
+                TSS = TSS |> round(2)) |>
+  dplyr::group_by(Modelo) |>
+  dplyr::mutate(modelID = 1:dplyr::n()) |>
+  dplyr::mutate(modelID = paste0(Modelo, "-", modelID)) |>
+  dplyr::rename("Id" = 2) |>
+  flextable::flextable() |>
+  flextable::align(align = "center", part = "all") |>
+  flextable::width(width = 1, j = 2) |>
+  flextable::fontsize(size = 12, part = "all")
+
+tabela_auc_tss_flex
 
 # Curva ROC ----
 
