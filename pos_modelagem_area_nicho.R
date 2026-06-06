e5747d1f18479b1f401966489c1ce3d6024647eb
Edbbioeco <edsonbbiologia@gmail.com>
2026-06-06 12:32:11 -0300

    Importar e visualizar o shapefile da Caatinga

--- pos_modelagem_area_nicho.R
+++ pos_modelagem_area_nicho.R
@@ -9,3 +9,18 @@ library(terra)
 library(tidyterra)
 
 library(ggview)
+
+# Dados ----
+
+## Shapefile da caatinga ----
+
+### Importar ----
+
+caa <- sf::st_read("shapefile_caatinga.shp")
+
+### Visualizar ----
+
+caa
+
+ggplot() +
+  geom_sf(data = caa, color = "black")
